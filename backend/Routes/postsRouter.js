d592a32845f9769e00419e85e42d0b14d307dcb5
LeaderOfMeow <ss826063@gmail.com>
2024-06-17 00:51:14 +0530

    authorization fixed

--- backend/Routes/postsRouter.js
+++ backend/Routes/postsRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const protected = require('../Middleware/protected');
 const postController = require('../Controller/postController');
-router.post('/newPost',protected,postController.newPost);
-router.get('/',protected,postController.getPost);
-router.get('/places',protected,postController.getPostById);
-router.put('/places',protected,postController.updatePost);
+router.post('/newPost',postController.newPost);
+router.get('/',postController.getPost);
+router.get('/places',postController.getPostById);
+router.put('/places',postController.updatePost);
 router.get('/getAllPost',postController.getAllPost);
 module.exports.router = router;
